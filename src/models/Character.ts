054b085a69348b44dca19f6c03b0e8c8e154a799
Matheus <matheusmgomes09@gmail.com>
2023-01-11 21:20:59 -0300

    [fix] collation added in order to query as case insensitive

--- src/models/Character.ts
+++ src/models/Character.ts
@@ -8,16 +8,19 @@ export interface ICharacter {
   level?: "civilian" | "student" | "genin" | "chuunin" | "jounin" | "kage";
 }
 
-const characterSchema = new mongoose.Schema<ICharacter>({
-  name: { type: String, required: true, index: true },
-  clan: { type: String, required: true, index: true },
-  summary: { type: String, required: true },
-  village: { type: String},
-  level: {
-    type: String,
-    enum: ["civilian", "student", "genin", "chuunin", "jounin", "kage"],
+const characterSchema = new mongoose.Schema<ICharacter>(
+  {
+    name: { type: String, required: true, index: true },
+    clan: { type: String, required: true, index: true },
+    summary: { type: String, required: true },
+    village: { type: String },
+    level: {
+      type: String,
+      enum: ["civilian", "student", "genin", "chuunin", "jounin", "kage"],
+    },
   },
-});
+  { collation: { locale: "en_US", strength: 1 } }
+);
 
 const characters = mongoose.model<ICharacter>("characters", characterSchema);
 
